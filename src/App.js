e7d6945301ced11f5703de04a2221009c748ceaf
hurima90-kim <hurima90@gmail.com>
2021-12-26 23:58:25 +0900

    feat: 자바스크립트 함수 스플리팅 예제

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const onClick = () => {
+    // import를 함수로 사용하면 promise를 반환한다.
+    // 모듈을 불러올때 모듈에서 default를 내보낸 것은 result.default를 참조해야 사용할 수 있다.
+    import("./notifty").then((result) => result.default());
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p onClick={onClick}>Hello React!</p>
       </header>
     </div>
   );
